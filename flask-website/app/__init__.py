43911a0f34d31112b24cfc08cbc33638b6dece02
Cloud User <centos@ip-172-31-56-127.ec2.internal>
2021-06-26 01:08:22 +0000

    database, register, login, service

---
+++ flask-website/app/__init__.py
@@ -0,0 +1,88 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, render_template
+from . import db
+from werkzeug.security import generate_password_hash
+from app.db import get_db
+
+
+app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
+load_dotenv()
+
+@app.route('/')
+def hello():
+    title = "Dakshina's Portfolio"
+    return render_template("index.html", title=title)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/experience')
+def experience():
+    return render_template("experience.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/health')
+def health():
+    return ('hi')
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
