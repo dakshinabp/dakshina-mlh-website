2295f5ff9d0e2c2a14c2e60706cba801302be073
Cloud User <centos@ip-172-31-56-127.ec2.internal>
2021-06-30 23:24:27 +0000

    postgres ex 2

--- flask-website/app/__init__.py
+++ flask-website/app/__init__.py
@@ -1,14 +1,39 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
-from . import db
+#from . import db
 from werkzeug.security import generate_password_hash
-from app.db import get_db
+#from app.db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 load_dotenv()
 
