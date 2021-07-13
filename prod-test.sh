0ef6a23c79a7fc1d4f4cf48d0f61aa62beb1fcd5
Dakshina Palasamudrum <dakshinabp@berkeley.edu>
2021-07-12 18:18:14 -0700

    prod.sh file

---
+++ prod-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+number="200"
+url=(https://dakshinapalas.duckdns.org, https://dakshinapalas.duckdns.org/register, https://dakshinapalas.duckdns.org/login)
+
+for i in ${url[@]}; do
+    status="$(curl ${url[@]} -I -o headers -s)"
+ 
+    response="$(cat headers | head -n 1 | cut '-d ' '-f2')"
+    if [ "${response}" -eq ${number} ] ; then
+        echo "0"
+        curl -s -X POST "$1" -d "content=🚀 Deployment Successful"
+    else
+        echo "1"
+        curl -s -X POST "$1" -d "content=😒 Deployment unsucessful"
+    fi
+done
